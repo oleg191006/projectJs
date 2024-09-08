6e7412c2bf2a3cef12671a57141a1785ed30b791
Олег bc2024-1 <olegchaplia2006@gmail.com>
2024-09-08 12:20:08 +0300

    Rewrite on a modular system

--- main.js
+++ main.js
@@ -6,9 +6,11 @@ const paperBtn = document.querySelector('.paper');
 const scissorsBtn = document.querySelector('.scissors');
 const resetGameBtn = document.querySelector('.reset');
 
+
 rockBtn.addEventListener('click', () => playRound('Rock'));
 paperBtn.addEventListener('click', () => playRound('Paper'));
 scissorsBtn.addEventListener('click', () => playRound('Scissors'));
+
 
 resetGameBtn.addEventListener('click', resetGame);
 
